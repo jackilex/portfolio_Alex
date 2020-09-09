6a1cee1cb80ab779e278c4a6b261325795fdde4a
Alex Saint Victor <jackilex91@gmail.com>
2020-09-08 22:02:40 -0500

    Updates

---
+++ precache-manifest.861e70d28c2c7532fac0b1c1f8c7a18a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6b716ad8fd793022cbe792f332a4262",
+    "url": "/portfolio_Alex/index.html"
+  },
+  {
+    "revision": "ac4e8145c6ecc5861438",
+    "url": "/portfolio_Alex/static/css/2.0fd4d61c.chunk.css"
+  },
+  {
+    "revision": "31bc9187654df4081350",
+    "url": "/portfolio_Alex/static/css/main.9f464ea5.chunk.css"
+  },
+  {
+    "revision": "ac4e8145c6ecc5861438",
+    "url": "/portfolio_Alex/static/js/2.1f2e1986.chunk.js"
+  },
+  {
+    "revision": "0e554cfde69e9c6960f4b62956156e6d",
+    "url": "/portfolio_Alex/static/js/2.1f2e1986.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31bc9187654df4081350",
+    "url": "/portfolio_Alex/static/js/main.c081ee26.chunk.js"
+  },
+  {
+    "revision": "11a22a055f3842d6804f",
+    "url": "/portfolio_Alex/static/js/runtime-main.3814f9da.js"
+  },
+  {
+    "revision": "821d027a8089e27073e2c8e789270a85",
+    "url": "/portfolio_Alex/static/media/photo.821d027a.jpg"
+  },
+  {
+    "revision": "e6723c488c49b8dfb5c950b9dfe9c6fd",
+    "url": "/portfolio_Alex/static/media/resume.e6723c48.pdf"
+  }
+]);
