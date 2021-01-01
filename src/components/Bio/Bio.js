085a4302e1226cc72a3c5974c115b51ca9584037
Alex Saint Victor <saintvictorcodes@gmail.com>
2021-01-01 02:12:38 -0600

    fixed title

--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -23,12 +23,12 @@ const Bio = () => {
               </div>
               <div className="paragraph">
               <p id="aboutMe"> <span id="nameBio"> My name is Alex Saint Victor{" "},<br /> I am a certified <span id="cert">Full-stack web developer{" "}</span>
-              from UT Austin with an interest in both front-end and back-end development. I have background
-             construction management and i have served in the US Marine Corps. I have a passion for creating, <span id="programming">programming and designing web applications.{" "}</span>
+              from UT Austin with an interest in both front-end and back-end development. I have a background
+             in construction management and i served in the US Marine Corps. I have a passion for creating, <span id="programming">programming and designing web applications.{" "}</span>
               </span>
               </p>
                 <div className="paragraph2">
-                <p><span id="nameBio"><span style={{textDecoration: "underline"}}> SKILLS & TECHNOLOGIES:</span> HTML5, CSS, JavaScript ES6+, React, Node.js, RESTful APIs, MySQL, MongoDB, Unit Testing, Integration testing, TDD Test</span></p>
+                <p><span id="nameBio"><span style={{textDecoration: "underline"}}> SKILLS & TECHNOLOGIES:</span> HTML5, CSS, JavaScript ES6+, React.js, Node.js, RESTful APIs, MySQL, MongoDB, Unit Testing, Integration testing, TDD Test</span></p>
                 </div>
               </div>
           </div>
